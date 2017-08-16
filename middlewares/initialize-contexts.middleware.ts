d9dfde132ab05cfe98d8a3cf9873f53f28bd588c
Orlando Quero <orlaqp@gmail.com>
2017-08-16 09:57:26 -0400

    created app context pool and review the app context creation and added some missing promises catchs

--- middlewares/initialize-contexts.middleware.ts
+++ middlewares/initialize-contexts.middleware.ts
@@ -1,21 +1,26 @@
+import { AppContextPool } from './app-context-pool';
+import { IAppModels } from '../data/models/app/app-models';
 import { IAccountDocument } from '../data/models';
 import { ExtendedRequest } from './extended-request';
 import { Request, Response } from 'express';
 import { getMasterContext, getContext, IIdentity } from '../data/models';
 import * as logger from 'winston';
+import * as mongoose from 'mongoose';
+
+const appContextPool = new AppContextPool();
 
 export function initializeContexts(req: ExtendedRequest, res: Response, next) {
 
-    let closeAppContext = function() {
-        if (req.appContext) {
-            if (req.appContext.Connection.readyState === 1) {
-                req.appContext.Connection.close();
-            }
-        }
-    };
+    // let closeAppContext = function() {
+    //     if (req.appContext) {
+    //         if (req.appContext.Connection.readyState === 1) {
+    //             req.appContext.Connection.close();
+    //         }
+    //     }
+    // };
 
-    res.once('finish', closeAppContext);
-    res.once('close', closeAppContext);
+    // res.once('finish', closeAppContext);
+    // res.once('close', closeAppContext);
 
     getMasterContext().then((ctx) => {
         req.masterContext = ctx;
@@ -28,15 +33,28 @@ export function initializeContexts(req: ExtendedRequest, res: Response, next) {
             logger.debug('creating app context from user hostname');
 
             ctx.Account.findAccountByHostname(hostname).then((account: IAccountDocument) => {
-                getContext(account.getConnectionString()).then((ctx) => {
+                // I not always need to create a new connection I may be able to re-use an existent one
+                appContextPool.getContext(account.getConnectionString()).then((ctx) => {
                     req.appContext = ctx;
                     next();
+                })
+                .catch(err => {
+                    logger.error('There was an error getting the app context', err);
+                    next();
                 });
+            })
+            .catch(err => {
+                logger.error('There was an error get account by hostname: ' + hostname, err);
+                next();
             });
         } else {
             logger.debug('no app context will be created for this request');
             next();
         }
+    })
+    .catch(err => {
+        logger.error('There was an error getting the master context', err);
+        next();
     });
 }
 
