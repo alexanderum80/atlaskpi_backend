87201f39db618191eed345ac4bff3f74089b8f7c
Raul Vallejo <raul@kpibi.com>
2017-09-02 17:14:37 -0400

    updating branch

--- activities/app/charts/preview-chart.activity.ts
+++ activities/app/charts/preview-chart.activity.ts
@@ -2,8 +2,8 @@ import { BasicRoleChecker } from '../basic-role-checker';
 import { IIdentity } from '../../../data';
 import { IActivity } from '../../../lib/enforcer';
 
-export const getChartActivity: IActivity = {
-    may: 'get-chart',
+export const previewChartActivity: IActivity = {
+    may: 'preview-chart',
     when(identity: IIdentity, cb: (err: any, authorized: boolean) => void) {
         cb(null, BasicRoleChecker.isAdmin(identity));
     }
