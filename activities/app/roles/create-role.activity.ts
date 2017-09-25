0130801c06246b57978d7b9c3b014acc77d68d1f
chris wright <professionalcandrew@gmail.com>
2017-09-25 15:14:36 -0400

    change request.identity to request.user for roles checking in activities

--- activities/app/roles/create-role.activity.ts
+++ activities/app/roles/create-role.activity.ts
@@ -4,11 +4,9 @@ import { IActivity } from '../../../lib/enforcer';
 export const createRoleActivity: IActivity = {
     may: 'create-role',
     when(request: ExtendedRequest, cb: (err: any, authorized: boolean) => void) {
-        let d = true;
-        let roles = request.identity.roles;
-        if (roles.indexOf('admin') === -1) {
-            d = false;
-        }
-        cb(null, d);
+        let checkRole = request.user.roles.find((role) => role.name === 'admin');
+        let hasPermission = checkRole ? true : false;
+
+        cb(null, hasPermission);
     }
 };
