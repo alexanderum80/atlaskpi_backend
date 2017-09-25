3a09a822a369a5ad2508441b890f68ad037ba8c0
chris wright <professionalcandrew@gmail.com>
2017-09-25 19:00:59 -0400

    modify activity to add basicrolechecker isadmin function

--- activities/app/roles/update-role.activity.ts
+++ activities/app/roles/update-role.activity.ts
@@ -1,12 +1,10 @@
+import { BasicRoleChecker } from '../basic-role-checker';
 import { ExtendedRequest } from '../../../middlewares/extended-request';
 import { IActivity } from '../../../lib/enforcer';
 
 export const updateRoleActivity: IActivity = {
     may: 'update-role',
     when(request: ExtendedRequest, cb: (err: any, authorized: boolean) => void) {
-        let checkRole = request.user.roles.find((role) => role.name === 'admin');
-        let hasPermission = checkRole ? true : false;
-
-        cb(null, hasPermission);
+        cb(null, BasicRoleChecker.isAdmin(request.user));
     }
 };
