ca595871cfbacbe4b64475a1a762e60a6bb5096c
chris wright <professionalcandrew@gmail.com>
2017-09-25 15:19:19 -0400

    modify create access log activity

--- activities/app/access-log/create-access-log.activity.ts
+++ activities/app/access-log/create-access-log.activity.ts
@@ -4,6 +4,6 @@ import { IActivity } from '../../../lib/enforcer';
 export const createAccessLogActivity: IActivity = {
     may: 'create-access-log',
     when(request: ExtendedRequest, cb: (err: any, authorized: boolean) => void) {
-        cb(cb, null);
+        cb(null, true);
     }
 }
