dcc08c956b4957aa4bffc54a7b0529575a732dbf
Orlando Quero <orlaqp@binaryways.com>
2017-10-15 16:21:36 -0400

    remove duplicated Permission definition from roles.gql

--- data/graphql/app/roles.gql.ts
+++ data/graphql/app/roles.gql.ts
@@ -22,11 +22,6 @@ export const rolesGql: GraphqlDefinition = {
                 permissions: [String]
                 timestamp: String
             }
-           type Permission {
-                _id: String
-                action: String
-                subject: String
-            }
             type Role {
                 _id: String
                 name: String
