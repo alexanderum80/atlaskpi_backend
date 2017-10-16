9ade4b4144a79387ceaeef1f64f8084f1a07075e
Orlando Quero <orlaqp@gmail.com>
2017-10-16 13:19:54 -0400

    fixed issue with gret charts by group activity

--- activities/app/charts/get-charts-groups.activity.ts
+++ activities/app/charts/get-charts-groups.activity.ts
@@ -5,6 +5,6 @@ import { IActivity } from '../../../lib/enforcer';
 export const getChartsGroupsActivity: IActivity = {
     may: 'get-charts-groups',
     when(request: ExtendedRequest, cb: (err: any, authorized: boolean) => void) {
-        cb(null, BasicRoleChecker.isAdmin(request.user));
+        cb(null, BasicRoleChecker.hasPermission(request.user, 'View' , 'Chart'));
     }
 };
