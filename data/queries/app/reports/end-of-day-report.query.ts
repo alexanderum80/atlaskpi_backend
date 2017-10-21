78a61f35987321559e7d502071114225a4a79019
Orlando Quero <orlaqp@binaryways.com>
2017-10-21 13:58:07 -0400

    ready for testing end of day report

--- data/queries/app/reports/end-of-day-report.query.ts
+++ data/queries/app/reports/end-of-day-report.query.ts
@@ -3,14 +3,11 @@ import { IIdentity } from '../../../models/app/identity';
 import { QueryBase } from '../../query-base';
 import * as Promise from 'bluebird';
 
-export class GetEndOfDayReportQuery extends QueryBase<IEndOfDayReport> {
+export class EndOfDayReportQuery extends QueryBase<IEndOfDayReport> {
 
     constructor(public identity: IIdentity, private _endOfDayReportService: EndOfDayReportService) {
         super(identity);
     }
-
-    // log = true;
-    // audit = true;
 
     run(data: any): Promise<IEndOfDayReport> {
         return this._endOfDayReportService.generateReport();
