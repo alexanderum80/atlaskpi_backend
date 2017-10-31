535228e29700601b7e7768b5d6862b64c984af5f
Orlando Quero <orlaqp@gmail.com>
2017-10-30 22:55:23 -0400

    added spline chart type

--- release.sh
+++ release.sh
@@ -4,3 +4,4 @@ cd dist
 docker build -t webapp-backend .
 docker tag webapp-backend:latest 288812438107.dkr.ecr.us-east-1.amazonaws.com/webapp-backend:latest
 docker push 288812438107.dkr.ecr.us-east-1.amazonaws.com/webapp-backend:latest
+cd ..
