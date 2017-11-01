fd01ee47b8f84398edf0a7b24d281bdb5f345336
Orlando Quero <orlaqp@gmail.com>
2017-11-01 15:49:08 -0400

    added a script to do releases

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npm run build
-cd dist
-docker build -t webapp-backend .
-docker tag webapp-backend:latest 288812438107.dkr.ecr.us-east-1.amazonaws.com/webapp-backend:latest
-docker push 288812438107.dkr.ecr.us-east-1.amazonaws.com/webapp-backend:latest
-cd ..
