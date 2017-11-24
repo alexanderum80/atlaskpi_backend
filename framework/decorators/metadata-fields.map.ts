a1b30015d36d2ebd9d28797f5cc796e29f154839
Orlando Quero <orlaqp@gmail.com>
2017-11-24 13:28:19 -0500

    saving module options info

--- framework/decorators/metadata-fields.map.ts
+++ framework/decorators/metadata-fields.map.ts
@@ -1,5 +1,6 @@
 export const MetadataFieldsMap = {
     Fields: '___fields___',
     Artifact: '___gqlArtifact___',
-    Definition: '___gqlDefinition___'
+    Definition: '___gqlDefinition___',
+    Types: '___gqlTypes___'
 };
