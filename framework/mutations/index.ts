633c071694772b0a6dc106867cbb26fea83ace75
Orlando Quero <orlaqp@binaryways.com>
2017-11-26 16:46:28 -0500

    bridge pieces are in place, now i need to fix query and mutation bus and start testing

--- framework/mutations/index.ts
+++ framework/mutations/index.ts
@@ -1,5 +1,5 @@
 export * from './mutation';
 export * from './validation-result';
 export * from './mutation-base';
-// export * from './mutation-bus';
+export * from './mutation-bus';
 // export * from './mutation-response';
