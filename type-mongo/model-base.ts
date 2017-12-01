163ca5ca13fdcfa64e5555c8c74693a9f50aac6a
Orlando Quero <orlaqp@binaryways.com>
2017-12-01 07:53:46 -0500

    I found a very simple way to convert current mongodb models into injectable classes

--- type-mongo/model-base.ts
+++ type-mongo/model-base.ts
@@ -1,15 +1,22 @@
 import * as mongoose from 'mongoose';
 import { injectable } from 'inversify';
-import { getModelMetadata } from './';
+import { getModelMetadata, DbConnection } from './';
 
 @injectable()
 export class ModelBase<T> {
     protected _schema: mongoose.Schema;
 
-    constructor(private _connection: mongoose.Connection) {
+    constructor(protected _connection: DbConnection,
+        protected modelName: string,
+        protected schema: mongoose.Schema,
+        protected collection: string) {
         // implement all methods from mongoose model
-        const metadata = getModelMetadata(this);
-        Object.assign(this, _connection.model(metadata.name, this._schema, metadata.collection));
+        // const metadata = getModelMetadata(this);
+        // Object.assign(this, _connection.model(metadata.name, this._schema, metadata.collection));
+    }
+
+    get model(): T {
+        return this._connection.get.model(this.modelName, this.schema, this.collection) as any;
     }
 
 }
