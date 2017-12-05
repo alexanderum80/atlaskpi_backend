6b1a6e37d33c5e17dfde7490a4e52407b7075adc
Orlando Quero <orlaqp@gmail.com>
2017-12-05 15:22:12 -0500

    date ranges refactored

--- src/app_modules/date-ranges/date-ranges.types.ts
+++ src/app_modules/date-ranges/date-ranges.types.ts
@@ -1,26 +1,22 @@
+import { field, GraphQLTypesMap, type } from '../../framework';
 
-    import { input, type, field, GraphQLTypesMap } from '../../framework';
+@type()
+export class DateRangeComparisonItem  {
+    @field({ type: GraphQLTypesMap.String })
+    key: string;
 
-    
-    @type()
-    export class DateRangeComparisonItem  {
-        @field({ type: GraphQLTypesMap.String })
-        key: string;
+    @field({ type: GraphQLTypesMap.String })
+    value: string;
 
-        @field({ type: GraphQLTypesMap.String })
-        value: string;
+}
 
-    }
-    
+@type()
+export class DateRangeResponse  {
+    @field({ type: ChartDateRange })
+    dateRange: ChartDateRange;
 
-    @type()
-    export class DateRangeResponse  {
-        @field({ type: ChartDateRange })
-        dateRange: ChartDateRange;
+    @field({ type: DateRangeComparisonItem, isArray: true })
+    comparisonItems: DateRangeComparisonItem[];
 
-        @field({ type: DateRangeComparisonItem, isArray: true })
-        comparisonItems: DateRangeComparisonItem[];
+}
 
-    }
-    
-    
