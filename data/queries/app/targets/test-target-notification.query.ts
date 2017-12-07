8c561c908bd680c86a9c9608ca79f6235fba9704
chris wright <professionalcandrew@gmail.com>
2017-12-07 11:55:10 -0500

    add target name, amount, date, dashboard name, and chart name to the email template

--- data/queries/app/targets/test-target-notification.query.ts
+++ data/queries/app/targets/test-target-notification.query.ts
@@ -1,3 +1,6 @@
+import { IUserDocument } from '../../../models/app/users/IUser';
+import { IDashboardModel } from '../../../models/app/dashboards/index';
+import { IChartDocument, IChartModel } from '../../../models/app/charts/index';
 import { INotify } from '../../../models/app/targets/ITarget';
 import { IUserModel } from '../../../models/app/users/index';
 import { IIdentity } from '../../../models/app/identity';
@@ -7,19 +10,43 @@ import * as Promise from 'bluebird';
 export class TestTargetNotificationQuery extends QueryBase<any> {
     constructor(public identity: IIdentity,
                 private _testNotification: any,
-                private _user: IUserModel) {
+                private _user: IUserModel,
+                private _chart: IChartModel,
+                private _dashboard: IDashboardModel) {
         super(identity);
     }
 
-    run(data: { input: {id: string[], targetId: string}}): Promise<any> {
+    // needs: targetName, targetAmount (target), targetDate (datepicker), dashboard name, chartName
+
+    run(data: { input: {usersId: string[], targetName: string, targetAmount: string, targetDate: string, chartId: string}}): Promise<any> {
         const that = this;
         const input = data.input;
         return new Promise<any>((resolve, reject) => {
-            that._user.findUsersById(input.id).then(users => {
-                users.forEach(user => {
-                    that._testNotification.notify(user, user.username, { id: input.targetId });
+            // query chart to get chart name
+            // use chart id to get the dashboard name
+
+            const chartQuery = that._chart.findById(input.chartId);
+            const dashboardQuery = that._dashboard.findDashboardByChartId(input.chartId);
+            const userQuery = that._user.findUsersById(input.usersId);
+
+            Promise.all([chartQuery, dashboardQuery, userQuery])
+                .spread((chart: IChartDocument, dashboard: string, users: IUserDocument[]) => {
+                    // pass data to the notification
+                    if (!chart || !dashboard || !users) {
+                        reject({field: 'target notification', errors: 'inefficient data'});
+                    }
+                    users.forEach(user => {
+                        const notifyData = {
+                            targetName: input.targetName,
+                            targetAmount: input.targetAmount,
+                            targetDate: input.targetDate,
+                            dashboardName: dashboard,
+                            chartName: chart.title
+                        };
+                        that._testNotification.notify(user, user.username, notifyData);
+                    });
                 });
-            });
+
         });
     }
 }
