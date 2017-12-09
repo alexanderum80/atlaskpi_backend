eb6e9bd84ff7721dd60efbbf5ce0aa2603045990
Orlando Quero <orlaqp@binaryways.com>
2017-12-09 08:42:26 -0500

    added identity to countri queries to avoid compilation issues

--- data/queries/master/countries/get-countries.query.ts
+++ data/queries/master/countries/get-countries.query.ts
@@ -1,10 +1,14 @@
 import { ICountry, ICountryModel } from '../../../models/master/countries/ICountry';
 import { IQuery } from '../..';
 import * as Promise from 'bluebird';
+import { QueryBase } from '../../query-base';
+import { IIdentity } from '../../../models/app/identity';
 
-export class GetCountriesQuery implements IQuery<ICountry[]> {
+export class GetCountriesQuery extends QueryBase<ICountry[]> {
 
-    constructor(private _CountryModel: ICountryModel) { }
+    constructor(public identity: IIdentity, private _CountryModel: ICountryModel) {
+        super(identity);
+    }
 
     run(data: any): Promise<ICountry[]> {
         return new Promise<ICountry[]>((resolve, reject) => {
--- data/queries/master/countries/get-states-for-country.query.ts
+++ data/queries/master/countries/get-states-for-country.query.ts
@@ -1,11 +1,14 @@
 import { IState, IStateModel } from '../../../models/master/countries';
 import { ICountry, ICountryModel } from '../../../models/master/countries/ICountry';
-import { IQuery } from '../..';
 import * as Promise from 'bluebird';
+import { IIdentity } from '../../../models/app/identity';
+import { QueryBase } from '../../query-base';
 
-export class GetStatesForCountryQuery implements IQuery<IState[]> {
+export class GetStatesForCountryQuery extends QueryBase<IState[]> {
 
-    constructor(private _StateModel: IStateModel) { }
+    constructor(public identity: IIdentity, private _StateModel: IStateModel) {
+        super(identity);
+    }
 
     run(country: string): Promise<IState[]> {
         return new Promise<IState[]>((resolve, reject) => {
