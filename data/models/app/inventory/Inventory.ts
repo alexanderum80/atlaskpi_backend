97421742be44c33768db6e70a18254e35e3b9bdb
Raul Vallejo <raul@kpibi.com>
2017-12-08 19:35:27 -0500

    inventory collection available on simple kpis

--- data/models/app/inventory/Inventory.ts
+++ data/models/app/inventory/Inventory.ts
@@ -1,30 +1,37 @@
-import { IInventoryModel } from './IInventory';
+import { IdName } from '../../common';
+import { IInventoryModel, IInventoryDocument } from './IInventory';
 import * as mongoose from 'mongoose';
 
 let Schema = mongoose.Schema;
-let InventorySchema = new Schema({
-    location: {
-        id: String,
-        name: String
-    },
-    customer: {
-        id: String,
-        name: String
-    },
-    employee: {
-        id: String,
-        name: String
-    },
-    product: {
-        id: String,
-        name: String
-    },
-    quantity: {
-        ordered: Number,
-        delivered: Number,
-        sold: Number
-    }
+
+const inventoryProductSchema = {
+    externalId: String,
+    itemCode: String,
+    itemDescription: String
+};
+
+export const InventorySchema = new Schema({
+    location: IdName,
+    product: inventoryProductSchema,
+    updatedAt: Date,
+    onHand: Number,
+    onOrder: Number,
+    cost: Number
 });
+
+InventorySchema.statics.findCriteria = function(field: string): Promise<string[]> {
+    const that = this;
+
+    return new Promise<string[]>((resolve, reject) => {
+        that.distinct(field).then(values => {
+            resolve(values);
+            return;
+        }).catch(err => {
+            reject(err);
+            return;
+        });
+    });
+};
 
 export function getInventoryModel(m: mongoose.Connection): IInventoryModel {
     return <IInventoryModel>m.model('Inventory', InventorySchema, 'inventory');
