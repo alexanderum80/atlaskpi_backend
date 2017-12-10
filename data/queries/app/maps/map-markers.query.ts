2a308be28b7766ecb755cc57eef0a3e7494a5fca
Orlando Quero <orlaqp@binaryways.com>
2017-12-09 20:10:58 -0500

    removed unnecesary curly bracket

--- data/queries/app/maps/map-markers.query.ts
+++ data/queries/app/maps/map-markers.query.ts
@@ -1,13 +1,28 @@
-import { IZipToMapModel } from '../../../models/master/zip-to-map/IZipToMap';
+import {
+    IZipToMapModel
+} from '../../../models/master/zip-to-map/IZipToMap';
 
 
-import { IIdentity } from '../../../models/app/identity';
-import { IQueryResponse } from '../../../models/common';
+import {
+    IIdentity
+} from '../../../models/app/identity';
+import {
+    IQueryResponse
+} from '../../../models/common';
 import * as Promise from 'bluebird';
-import { IQuery } from '../..';
-import { ISaleModel, TypeMap } from '../../../models/app/sales/index';
-import { QueryBase } from '../../../index';
-import { keyBy } from 'lodash';
+import {
+    IQuery
+} from '../..';
+import {
+    ISaleModel,
+    TypeMap
+} from '../../../models/app/sales/index';
+import {
+    QueryBase
+} from '../../../index';
+import {
+    keyBy
+} from 'lodash';
 
 export interface IMapMarker {
     name: string;
@@ -17,39 +32,44 @@ export interface IMapMarker {
     value: number;
 }
 
-export class MapMarkersQuery extends QueryBase<IMapMarker[]> {
+export class MapMarkersQuery extends QueryBase < IMapMarker[] > {
     constructor(public identity: IIdentity,
-                private _SaleModel: ISaleModel,
-                private _ZipToMapModel: IZipToMapModel) {
-                    super(identity);
-                }
+        private _SaleModel: ISaleModel,
+        private _ZipToMapModel: IZipToMapModel) {
+        super(identity);
+    }
 
-    run(data: { type: TypeMap }): Promise<IMapMarker[]> {
+    run(data: {
+        type: TypeMap
+    }): Promise < IMapMarker[] > {
         const that = this;
 
-        return new Promise<IMapMarker[]>((resolve, reject) => {
+        return new Promise < IMapMarker[] > ((resolve, reject) => {
             this._SaleModel.salesBy(data.type).then(salesByZip => {
-                // get the zip codes related
-                that._ZipToMapModel.find({ zipCode: { $in: salesByZip.map(d => d._id) } })
-                    .then(zipList => {
-                        // convert array to object
-                        const salesObject = keyBy(salesByZip, '_id');
+                    // get the zip codes related
+                    that._ZipToMapModel.find({
+                            zipCode: {
+                                $in: salesByZip.map(d => d._id)
+                            }
+                        })
+                        .then(zipList => {
+                            // convert array to object
+                            const salesObject = keyBy(salesByZip, '_id');
 
-                        const markers = zipList.map(zip => {
-                            return {
-                                name: zip.zipCode,
-                                lat: zip.lat,
-                                lng: zip.lng,
-                                color: 'green',
-                                value: salesObject[zip.zipCode].sales
-                            };
+                            const markers = zipList.map(zip => {
+                                return {
+                                    name: zip.zipCode,
+                                    lat: zip.lat,
+                                    lng: zip.lng,
+                                    color: 'green',
+                                    value: salesObject[zip.zipCode].sales
+                                };
+                            });
+
+                            resolve(markers);
                         });
-
-                        resolve(markers);
-                    });
                 })
                 .catch(err => reject(err));
-            });
-        }
+        });
     }
 }
