6718a51dbd63c56b714c465e121f34770b794b7b
Orlando Quero <orlaqp@gmail.com>
2017-12-15 12:32:04 -0500

    controllers fixed

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,6 +1,12 @@
-import { IAccountModel, Accounts, IUserToken, IAccountDocument, IUserDocument, Users } from '../domain';
-import { injectable, inject } from 'inversify';
+import * as Promise from 'bluebird';
+import { inject, injectable } from 'inversify';
 import { Winston } from 'winston';
+
+import { IUserDocument } from '../domain/app/security/users/user';
+import { IUserToken } from '../domain/app/security/users/user-token';
+import { Users } from '../domain/app/security/users/user.model';
+import { IAccountDocument } from '../domain/master/accounts/Account';
+import { Accounts } from '../domain/master/accounts/account.model';
 
 @injectable()
 export class AuthController {
@@ -11,7 +17,7 @@ export class AuthController {
         @inject('logger') private _logger: Winston,
         @inject('Users') private _users: Users) {}
 
-    authenticateUser(hostname: string, username: string, password: string, ip: string, clientId: string, clientDetails: string): Promise < IUserToken > {
+    authenticateUser(hostname: string, username: string, password: string, ip: string, clientId: string, clientDetails: string, secret: string, expiresIn: string): Promise < IUserToken > {
         let that = this;
 
         return new Promise <IUserToken> ((resolve, reject) => {
@@ -39,7 +45,7 @@ export class AuthController {
                     return that._users.model.authenticate(username, password);
                 })
                 .then((u: IUserDocument) => {
-                    return u.generateToken(account.database.name, username, password, ip, clientId, clientDetails).then(token => {
+                    return u.generateToken(account.database.name, username, password, ip, clientId, clientDetails, secret, expiresIn).then(token => {
                         // add the rest of the info to the user token
                         token.subdomain = hostname.split('.')[0];
                         token.email = username;
