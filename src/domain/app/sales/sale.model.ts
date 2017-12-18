8a18af32c01739994e9bc36d58a041e9273ee42b
chris wright <professionalcandrew@gmail.com>
2017-12-18 09:29:17 -0500

    add method from another branch

--- src/domain/app/sales/sale.model.ts
+++ src/domain/app/sales/sale.model.ts
@@ -135,6 +135,20 @@ SalesSchema.statics.findByPredefinedDateRange = function(predefinedDateRange: st
     });
 };
 
+SalesSchema.statics.findCriteria = function(field: string): Promise<any[]> {
+    const that = this;
+
+    return new Promise<any[]>((resolve, reject) => {
+        that.distinct(field).then(sales => {
+            resolve(sales);
+            return;
+        }).catch(err => {
+            reject(err);
+            return;
+        });
+    });
+};
+
 @injectable()
 export class Sales extends ModelBase<ISaleModel> {
     constructor(@inject(AppConnection.name) appConnection: AppConnection) {
