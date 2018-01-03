39fd534e961f91b31796fa6a27c3e04cc5d7e9eb
Raul Vallejo <raul@kpibi.com>
2018-01-03 12:36:09 -0500

    fix send email when a new user is created

--- src/services/notifications/users/account-created.notification.ts
+++ src/services/notifications/users/account-created.notification.ts
@@ -7,6 +7,7 @@ import * as nodemailer from 'nodemailer';
 import { IAppConfig } from '../../../configuration/config-models';
 import { IAccountCreatedDataSource, IUserDocument } from '../../../domain/app/security/users/user';
 import { IEmailNotifier } from '../email-notifier';
+import { CurrentAccount } from '../../../domain/master/current-account';
 
 
 export interface IAccountCreatedNotifier extends IEmailNotifier {
@@ -16,16 +17,17 @@ export interface IAccountCreatedNotifier extends IEmailNotifier {
 @injectable()
 export class AccountCreatedNotification implements IAccountCreatedNotifier {
 
-    constructor(@inject('Config') private _config: IAppConfig) { }
+    constructor(@inject('Config') private _config: IAppConfig,
+                @inject(CurrentAccount.name) private _currentAccount: CurrentAccount) { }
 
-    notify(user: IUserDocument, hostname: string, email?: string, data?: any): Promise<nodemailer.SentMessageInfo> {
+    notify(user: IUserDocument, email: string, data?: any): Promise<nodemailer.SentMessageInfo> {
 
         const createAccountTemplate =
             Handlebars.compile(this._config.usersService.services.createUser.emailTemplate);
 
         let dataSource: IAccountCreatedDataSource = user.toObject();
         if (!dataSource.host) {
-            dataSource.host = hostname;
+            dataSource.host = this._currentAccount.get.database.name;
         }
         if (!dataSource.subdomain) {
             dataSource.subdomain = this._config.subdomain;
