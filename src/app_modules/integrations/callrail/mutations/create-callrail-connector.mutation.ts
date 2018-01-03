dcedd6bbf76d41e29af6d1f355f717ce6f61082a
chris wright <professionalcandrew@gmail.com>
2018-01-03 13:15:54 -0500

    inject currentaccount and callrail service to getusername, create connection object and add connector

--- src/app_modules/integrations/callrail/mutations/create-callrail-connector.mutation.ts
+++ src/app_modules/integrations/callrail/mutations/create-callrail-connector.mutation.ts
@@ -1,3 +1,7 @@
+import { CurrentAccount } from '../../../../domain/master/current-account';
+import { CallRailService } from '../../../../services/callrail.services';
+import { ConnectorTypeEnum, getConnectorTypeId } from '../../models/connector-type';
+import { IConnector } from '../../../../domain/master/connectors/connector';
 import { CallRailInput, CallRailResponse } from '../callrail.types';
 import { CreateCallRailConnectorActivity } from '../activities/create-callrail-connector.activity';
 import { inject, injectable } from 'inversify';
@@ -5,6 +9,7 @@ import * as Promise from 'bluebird';
 import { mutation } from '../../../../framework/decorators/mutation.decorator';
 import { MutationBase } from '../../../../framework/mutations/mutation-base';
 import { IMutationResponse } from '../../../../framework/mutations/mutation-response';
+import { Connectors } from '../../../../domain/master/connectors/connector.model';
 
 @mutation({
     name: 'callRailConnect',
@@ -17,12 +22,16 @@ import { IMutationResponse } from '../../../../framework/mutations/mutation-resp
 
 @injectable()
 export class CreateCallRailConnectorMutation extends MutationBase<IMutationResponse> {
-    constructor() {
+    constructor(
+                @inject(Connectors.name) private _connectorModel: Connectors,
+                @inject(CallRailService.name) private _callrailService: CallRailService,
+                @inject(CurrentAccount.name) private _currentAccount: CurrentAccount) {
         super();
     }
 
-    run(input: CallRailInput): Promise<IMutationResponse> {
+    run(data: { input: CallRailInput }): Promise<IMutationResponse> {
         const that = this;
+        const input = data.input;
         return new Promise<IMutationResponse>((resolve, reject) => {
             if (!input) {
                 reject({ success: false, errors: [{ field: 'input', errors: ['No data provided'] }] });
@@ -34,7 +43,36 @@ export class CreateCallRailConnectorMutation extends MutationBase<IMutationRespo
                 return;
             }
 
-            
+            that._callrailService.getUserName(that._connectorModel, input).then(callRailUser => {
+                const connObj: IConnector = {
+                    name: callRailUser.name, // karl smith, query to get name
+                    active: true,
+                    config: {
+                        token: {
+                            accountId: input.accountId,
+                            apiKey: input.apiKey
+                        }
+                    },
+                    databaseName: that._currentAccount.get.database.name, // local database name
+                    type: getConnectorTypeId(ConnectorTypeEnum.CallRail),
+                    createdBy: 'backend',
+                    createdOn: new Date(Date.now()),
+                    uniqueKeyValue: {
+                        key: 'config.token.apiKey',
+                        value: input.apiKey
+                    }
+                };
+                that._connectorModel.model.addConnector(connObj).then(() => {
+                    resolve({ success: true, entity: null });
+                    return;
+                }).catch(err => {
+                    reject({ success: false, errors: [{ field: 'connector', errors: ['Unable to add connector'] }] });
+                    return;
+                });
+            });
+
+
+
         });
     }
 }
