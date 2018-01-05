2b464b0a0806e058b952e69f35333b5d57b2bd48
chris wright <professionalcandrew@gmail.com>
2018-01-05 18:26:58 -0500

    check if widget is using a chart before deleting a chart

--- src/app_modules/charts/mutations/delete-chart.mutation.ts
+++ src/app_modules/charts/mutations/delete-chart.mutation.ts
@@ -1,3 +1,4 @@
+import { Widgets } from '../../../domain/app/widgets/widget.model';
 import * as Promise from 'bluebird';
 import { inject, injectable } from 'inversify';
 
@@ -22,24 +23,47 @@ import { Logger } from './../../../domain/app/logger';
 export class DeleteChartMutation extends MutationBase<IMutationResponse> {
     constructor(
         @inject(ChartsService.name) private _chartsService: ChartsService,
-        @inject(Logger.name) private _logger: Logger) {
+        @inject(Logger.name) private _logger: Logger,
+        @inject(Widgets.name) private _widgetModel: Widgets) {
         super();
     }
 
     run(data: { id: string }): Promise<IMutationResponse> {
         const that = this;
         return new Promise<IMutationResponse>((resolve, reject) => {
-            that._chartsService
-                .deleteChart(data.id)
-                .then(chart => {
+
+            const findWidget = that._widgetModel.model.find({
+                'type': 'chart',
+                'chartWidgetAttributes.chart': data.id
+            });
+
+            const models = {
+                widget: findWidget
+            };
+
+            Promise.props(models).then(documents => {
+                if (documents.widget && documents.widget.length) {
+                    resolve({
+                        success: false,
+                        errors: [
+                            {
+                                field: '__isDependencyOf',
+                                errors: documents.widget.map(document => `Widget: ${document.name}`)
+                            }
+                          ]
+                        });
+                    return;
+                }
+
+                that._chartsService.deleteChart(data.id).then(chart => {
                     resolve({ success: true, entity: chart });
                     return;
-                })
-                .catch(err => {
+                }).catch(err => {
                     that._logger.error(err);
                     resolve({ success: false, errors: [ { field: 'id', errors: [err]} ] });
                     return;
                 });
+            });
         });
     }
 }
