4795d13f221e33944774ac8b760d376d83ad8ead
chris wright <professionalcandrew@gmail.com>
2018-01-05 10:19:09 -0500

    make changes recommended

--- src/services/callrail.services.ts
+++ src/services/callrail.services.ts
@@ -27,69 +27,67 @@ export class CallRailService {
 
     private _config: any;
 
-    constructor() {}
-    hasValidCredentialsTest(connectorModel: Connectors, input: ICallRailInput): Promise<boolean> {
+    constructor(@inject(Connectors.name) private _connectorModel: Connectors) {}
+
+    public initialize(): Promise<any> {
         const that = this;
-        return new Promise<boolean>((resolve, reject) => {
-            loadIntegrationConfig(connectorModel, 'callrail').then(configDoc => {
+        return new Promise<any>((resolve, reject) => {
+            loadIntegrationConfig(that._connectorModel, 'callrail').then(configDoc => {
                 if (!configDoc) {
                     reject('connect configuration not found');
                     return;
                 }
 
                 that._config = configDoc.config;
-                const url = `${that._config.callrailIntegration.endpoint}/${input.accountId}/${that._config.usersApiUrl}`;
-                const headers = {
-                    Authorization: 'Token token=' + input.apiKey
-                };
+                resolve();
+                return;
+            }).catch(err => reject(err));
+        });
+    }
+    validateCredentials(input: ICallRailInput): Promise<boolean> {
+        const that = this;
+        return new Promise<boolean>((resolve, reject) => {
+            const url = `${that._config.callrailIntegration.endpoint}/${input.accountId}/${that._config.usersApiUrl}`;
+            const headers = {
+                Authorization: 'Token token=' + input.apiKey
+            };
 
-                request({
-                    url: url,
-                    method: 'GET',
-                    headers: headers
-                }, (error, response, body) => {
-                    const data = JSON.parse(body);
-                    resolve(data.error ? false : true);
-                    return;
-                });
+            request({
+                url: url,
+                method: 'GET',
+                headers: headers
+            }, (error, response, body) => {
+                const data = JSON.parse(body);
+                resolve(data.error ? false : true);
+                return;
             });
         });
 
     }
-    getUserName(connectorModel: Connectors, input: ICallRailInput): Promise<IGetUserNameResponse> {
+    getUserName(input: ICallRailInput): Promise<IGetUserNameResponse> {
         const that = this;
 
         return new Promise<IGetUserNameResponse>((resolve, reject) => {
-            loadIntegrationConfig(connectorModel, 'callrail').then(configDoc => {
-                if (!configDoc) {
-                    reject('connect configuration not found');
+            const url = `${that._config.callrailIntegration.endpoint}/${input.accountId}/${that._config.usersApiUrl}`;
+            const headers = {
+                Authorization: 'Token token=' + input.apiKey
+            };
+
+            request({
+                url: url,
+                method: 'GET',
+                headers: headers
+            }, (error, response, body) => {
+                const data = JSON.parse(body);
+                const findAdminUser = data.users.find(d => d.role === 'admin');
+                if (!findAdminUser) {
+                    resolve({ name: 'callrail' });
                     return;
                 }
 
-                that._config = configDoc.config;
-                const url = `${that._config.callrailIntegration.endpoint}/${input.accountId}/${that._config.usersApiUrl}`;
-                const headers = {
-                    Authorization: 'Token token=' + input.apiKey
-                };
-
-                request({
-                    url: url,
-                    method: 'GET',
-                    headers: headers
-                }, (error, response, body) => {
-                    const data = JSON.parse(body);
-                    const findAdminUser = data.users.find(d => d.role === 'admin');
-                    if (!findAdminUser) {
-                        resolve({ name: 'callrail' });
-                        return;
-                    }
-
-                    resolve({ name: findAdminUser.name });
-                    return;
-                });
-
+                resolve({ name: findAdminUser.name });
+                return;
             });
-
         });
 
     }
