56183b8e70ab23d917300738d11bbe2d3aadb6c2
chris wright <professionalcandrew@gmail.com>
2018-01-09 17:37:38 -0500

    fix the custom date range and add injectable to callrail activity

--- src/app_modules/charts/queries/charts/ui-chart-base.ts
+++ src/app_modules/charts/queries/charts/ui-chart-base.ts
@@ -340,7 +340,7 @@ export class UIChartBase {
             /**
              *  this is a one level grouping chart
              */
-            return this._getSeriesForFirstLevelGrouping(data, categories, meta.xAxisSource);
+            return this._getSeriesForFirstLevelGrouping(data, categories, meta);
 
 
         } else if (availableGroupingsForSeries.length === 1) {
@@ -362,14 +362,14 @@ export class UIChartBase {
     }
 
 
-    private _getSeriesForFirstLevelGrouping(data: any[], categories: IXAxisCategory[], group: string): IChartSerie[] {
+    private _getSeriesForFirstLevelGrouping(data: any[], categories: IXAxisCategory[], meta: IChartMetadata): IChartSerie[] {
 
         if (this.chart.chartDefinition.chart.type === ChartType.Pie) {
             return [{
                 name: '',
                 data:  categories.map(cat => {
                     let dataItem = data.find((item: any) => {
-                        return item._id[group] === cat.id;
+                        return item._id[meta.xAxisSource] === cat.id;
                     });
 
                     return {
@@ -378,9 +378,10 @@ export class UIChartBase {
                     };
                 })
             }];
-        } else if (group) {
-            const groupedData: Dictionary<any> = groupBy(data, '_id.' + group);
-            return this._createSeriesFromgroupedData(groupedData, categories, group);
+        } else if (meta.xAxisSource) {
+            const groupedData: Dictionary<any> = groupBy(data, '_id.' + meta.xAxisSource);
+            const matchField = getFrequencyPropName(meta.frequency);
+            return this._createSeriesFromgroupedData(groupedData, categories, matchField);
         } else {
             return [{
                 name: '',
