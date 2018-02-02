6e97b4071db9d2d2b410ea726af54e154bacb22c
chris wright <professionalcandrew@gmail.com>
2018-02-02 10:45:44 -0500

    move code to check if kpi is in use inside a service. refractor remove kpi mutation class

--- src/app_modules/kpis/mutations/remove-kpi.mutation.ts
+++ src/app_modules/kpis/mutations/remove-kpi.mutation.ts
@@ -1,11 +1,8 @@
+import { KpiService } from '../../../services/kpi.service';
 import { IKPIDocument } from '../../../domain/app/kpis/kpi';
-import { IWidgetDocument } from '../../../domain/app/widgets/widget';
-import { IChartDocument } from '../../../domain/app/charts/chart';
-import { Widgets } from '../../../domain/app/widgets/widget.model';
 import * as Promise from 'bluebird';
 import { inject, injectable } from 'inversify';
 
-import { Charts } from '../../../domain/app/charts/chart.model';
 import { KPIs } from '../../../domain/app/kpis/kpi.model';
 import { field } from '../../../framework/decorators/field.decorator';
 import { mutation } from '../../../framework/decorators/mutation.decorator';
@@ -26,8 +23,7 @@ import { KPIRemoveResponse } from '../kpis.types';
 export class RemoveKpiMutation extends MutationBase<IMutationResponse> {
     constructor(
         @inject(KPIs.name) private _kpis: KPIs,
-        @inject(Charts.name) private _charts: Charts,
-        @inject('Widgets') private _widgets: Widgets
+        @inject(KpiService.name) private _kpiService: KpiService
     ) {
         super();
     }
@@ -36,39 +32,13 @@ export class RemoveKpiMutation extends MutationBase<IMutationResponse> {
         const that = this;
 
         return new Promise<IMutationResponse>((resolve, reject) => {
-            if (!data.id) {
-                return reject({ success: false,
-                                errors: [ { field: 'id', errors: ['Chart not found']} ] });
-            }
-
-            const findCharts = this._charts.model.find({ kpis: { $in: [data.id] } });
-
-            const findWidgets = this._widgets.model.find({
-                'numericWidgetAttributes.kpi': data.id
+            that._kpiService.removeKpi(data.id).then(result => {
+                resolve(result);
+                return;
+            }).catch(err => {
+                resolve({ success: false, entity: err.entity, errors: [ { field: 'kpi', errors: [ err.error ] }]});
+                return;
             });
-
-            const expression = new RegExp(data.id);
-            const findComplexKpi = this._kpis.model.find({
-                expression: {
-                    $regex: expression
-                }
-            });
-
-            let documentExists: any = {};
-            return Promise.all([findCharts, findWidgets, findComplexKpi])
-                .spread((charts: IChartDocument[], widgets: IWidgetDocument[], complexKpis: IKPIDocument[]) => {
-                    documentExists.chart = charts;
-                    documentExists.widget = widgets;
-                    documentExists.complexKPI = complexKpis;
-
-                    return that._kpis.model.removeKPI(data.id, documentExists).then(foundDocument => {
-                        resolve({ success: true, entity: foundDocument });
-                        return;
-                    });
-                }).catch(err => {
-                    resolve({ success: false, entity: err.entity, errors: [ { field: 'kpi', errors: [err.error]}]});
-                    return;
-                });
         });
     }
 }
--- src/services/kpi.service.ts
+++ src/services/kpi.service.ts
@@ -1,15 +1,20 @@
+import { IWidgetDocument } from '../domain/app/widgets/widget';
+import { IChartDocument } from '../domain/app/charts/chart';
+import { Widgets } from '../domain/app/widgets/widget.model';
+import { Charts } from '../domain/app/charts/chart.model';
 import { GroupingMap } from '../app_modules/charts/queries/chart-grouping-map';
 import { KPIs } from '../domain/app/kpis/kpi.model';
 import { Inventory } from '../domain/app/inventory/inventory.model';
 import { Expenses } from '../domain/app/expenses/expense.model';
 import { Sales } from '../domain/app/sales/sale.model';
-import { IKPIDataSourceHelper, IKPIDocument } from '../domain/app/kpis/kpi';
+import { IDocumentExist, IKPIDataSourceHelper, IKPIDocument } from '../domain/app/kpis/kpi';
 import * as Promise from 'bluebird';
 import { inject, injectable } from 'inversify';
 import {
     sortBy,
     isObject
 } from 'lodash';
+import * as mongoose from 'mongoose';
 
 const codeMapper = {
     'Revenue': 'sales',
@@ -22,7 +27,9 @@ export class KpiService {
         @inject(Sales.name) private _saleModel: Sales,
         @inject(Expenses.name) private _expenseModel: Expenses,
         @inject(Inventory.name) private _inventoryModel,
-        @inject(KPIs.name) private _kpis: KPIs
+        @inject(KPIs.name) private _kpis: KPIs,
+        @inject(Charts.name) private _chart: Charts,
+        @inject(Widgets.name) private _widget: Widgets
     ) {}
 
     GetGroupingsExistInCollectionSchema(schemaName: string): Promise<any> {
@@ -70,6 +77,76 @@ export class KpiService {
                     return resolve(sortBy(permittedFields));
                 }
             });
+        });
+    }
+
+    removeKpi(id: string): Promise<any> {
+        const that = this;
+
+        return new Promise<any>((resolve, reject) => {
+            // do something
+            that._kpiInUseByModel(id).then(res => {
+                // check if kpi is in use by another model
+                const { chart, widget, complexKPI } = res;
+                const modelExists: number = chart.length || widget.length || complexKPI.length;
+
+                if (modelExists) {
+                    reject({ message: 'KPIs is being used by ', entity: res, error: 'KPIs is being used by '});
+                    return;
+                }
+
+                // remove kpi when not in use
+                that._kpis.model.removeKPI(id).then(document => {
+                    resolve(document);
+                    return;
+                }).catch(err => {
+                    reject(err);
+                    return;
+                });
+            }).catch(err => {
+                reject(err);
+                return;
+            });
+        });
+    }
+
+    private _kpiInUseByModel(id: string): Promise<IDocumentExist> {
+        const that = this;
+
+        return new Promise<IDocumentExist>((resolve, reject) => {
+            if (!id) {
+                return reject({ success: false,
+                                errors: [ { field: 'id', errors: ['Chart not found']} ] });
+            }
+
+            const findCharts = this._chart.model.find({ kpis: { $in: [id] } });
+            const findWidgets = this._widget.model.find({
+                'numericWidgetAttributes.kpi': id
+            });
+
+            const expression = new RegExp(id);
+            const findComplexKpi = this._kpis.model.find({
+                expression: {
+                    $regex: expression
+                }
+            });
+
+            let documentExists: any = {};
+
+            Promise.all([findCharts, findWidgets, findComplexKpi])
+                .spread((charts: IChartDocument[], widgets: IWidgetDocument[], complexKPI: IKPIDocument[]) => {
+                    documentExists = {
+                        chart: charts,
+                        widget: widgets,
+                        complexKPI
+                    };
+
+                    resolve(documentExists);
+                    return;
+                }).catch(err => {
+                    reject(err);
+                    return;
+                });
         });
     }
 
