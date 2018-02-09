13d130f6d054cff7afc877f348c22ae63e50a935
chris wright <professionalcandrew@gmail.com>
2018-02-09 14:50:26 -0500

    comment out untitled file

--- src/app_modules/locations/queries/Untitled-1.ts
+++ src/app_modules/locations/queries/Untitled-1.ts
@@ -1,81 +1,81 @@
-import * as Promise from 'bluebird';
-import { inject, injectable } from 'inversify';
+// import * as Promise from 'bluebird';
+// import { inject, injectable } from 'inversify';
 
-import { ILocationDocument } from '../../../domain/app/location/location';
-import { Locations } from '../../../domain/app/location/location.model';
-import { query } from '../../../framework/decorators/query.decorator';
-import { IQuery } from '../../../framework/queries/query';
-import { LocationByIdActivity } from '../activities/location-by-id.activity';
-import { Location } from '../locations.types';
+// import { ILocationDocument } from '../../../domain/app/location/location';
+// import { Locations } from '../../../domain/app/location/location.model';
+// import { query } from '../../../framework/decorators/query.decorator';
+// import { IQuery } from '../../../framework/queries/query';
+// import { LocationByIdActivity } from '../activities/location-by-id.activity';
+// import { Location } from '../locations.types';
 
-@input()
-export class SearchInput {
+// @input()
+// export class SearchInput {
 
-    @field({ type: GraphqlTypeMap.String })
-    city: string;
+//     @field({ type: GraphqlTypeMap.String })
+//     city: string;
 
-    @field({ type: GraphqlTypeMap.String })
-    state: string;
+//     @field({ type: GraphqlTypeMap.String })
+//     state: string;
 
-    @field({ type: GraphqlTypeMap.String })
-    name: string;
+//     @field({ type: GraphqlTypeMap.String })
+//     name: string;
 
-}
+// }
 
-/**
- * Create a location search that allow the following search criterias:
- * 
- * city
- * state
- * name
- * 
- */
+// /**
+//  * Create a location search that allow the following search criterias:
+//  * 
+//  * city
+//  * state
+//  * name
+//  * 
+//  */
 
-@injectable()
-@query({
-    name: 'searchLocation',
-    activity: SearchLocationsActivity,
-    parameters:[
-        { name: 'searchInput', type: SearchInput }
-    ]
+// @injectable()
+// @query({
+//     name: 'searchLocation',
+//     activity: SearchLocationsActivity,
+//     parameters:[
+//         { name: 'searchInput', type: SearchInput }
+//     ]
   
-})
-export class LocationByIdQuery implements IQuery<ILocationDocument> {
-    constructor(@inject(Locations.name) private _locations: Locations) { }
+// })
+// export class LocationByIdQuery implements IQuery<ILocationDocument> {
+//     constructor(@inject(Locations.name) private _locations: Locations) { }
 
-    run(data: { searchInput: SearchInput }): Promise<ILocationDocument> {
+//     run(data: { searchInput: SearchInput }): Promise<ILocationDocument> {
         
-        return this._locations.model.search(data.searchInput);
+//         return this._locations.model.search(data.searchInput);
 
 
 
 
-    }
-}
+//     }
+// }
 
 
-// Location Model
+// // Location Model
 
-function search(filters: SearchInput) {
-    const conditions = {} as any;
+// function search(filters: SearchInput) {
+//     const conditions = {} as any;
 
-    if (filters.city) {
-        conditions.city = filters.city;
-    }
+//     if (filters.city) {
+//         conditions.city = filters.city;
+//     }
 
-    if (filters.state) {
-        conditions.state = filters.state;
-    }
+//     if (filters.state) {
+//         conditions.state = filters.state;
+//     }
 
-    if (filters.name) {
-        conditions.name = {
-            $regex: new RegExp(filters.name)
-        };
-    }
+//     if (filters.name) {
+//         conditions.name = {
+//             $regex: new RegExp(filters.name)
+//         };
+//     }
 
-    this.find(conditions).then(result => {
+//     this.find(conditions).then(result => {
 
-    });
-}
+//     });
+// }
 
 
