9fe3183c70638a005870ac4d76291afca42d5dae
chris wright <professionalcandrew@gmail.com>
2018-03-28 09:01:39 -0400

    fix bug after changing the naming convention for the alert model

--- src/domain/app/alerts/alert.model.ts
+++ src/domain/app/alerts/alert.model.ts
@@ -57,7 +57,7 @@ AlertSchema.statics.alertByWidgetId = function(id: string): Promise<IAlertDocume
     const alertModel = (<IAlertModel>this);
 
     return new Promise<IAlertDocument>((resolve, reject) => {
-        alertModel.findOne({ model_id: id })
+        alertModel.findOne({ 'model.id': id })
             .then((result: IAlertDocument) => {
                 resolve(result);
                 return;
@@ -148,7 +148,7 @@ AlertSchema.statics.removeAlertByModelId = function(id: string): Promise<IAlertD
             return;
         }
 
-        alertModel.findOne({ model_id: id }).then((alert: IAlertDocument) => {
+        alertModel.findOne({ 'model.id': id }).then((alert: IAlertDocument) => {
             if (!alert) {
                 resolve(null);
                 return;
