fbf57cbb687aa7be3c8bab00787feb29e786b516
chris wright <professionalcandrew@gmail.com>
2018-04-20 12:12:50 -0400

    remove code to apply top n from kpi base class

--- src/app_modules/kpis/queries/kpi-base.ts
+++ src/app_modules/kpis/queries/kpi-base.ts
@@ -1,13 +1,13 @@
 import * as Bluebird from 'bluebird';
 import { camelCase } from 'change-case';
-import { cloneDeep, flatten, groupBy, isArray, isDate, isNumber, isObject, pick, reduce, sortBy } from 'lodash';
+import { cloneDeep, groupBy, isArray, isDate, isNumber, isObject } from 'lodash';
 import * as moment from 'moment';
 import * as logger from 'winston';
 
 import { IKPI } from '../../../domain/app/kpis/kpi';
 import { IChartDateRange, IDateRange } from '../../../domain/common/date-range';
 import { FrequencyEnum } from '../../../domain/common/frequency-enum';
-import { chartTopMomentFormat, chartTopValue, IChartTop } from '../../../domain/common/top-n-record';
+import { IChartTop } from '../../../domain/common/top-n-record';
 import { field } from '../../../framework/decorators/field.decorator';
 import { isArrayObject } from '../../../helpers/express.helpers';
 import { NULL_CATEGORY_REPLACEMENT } from '../../charts/queries/charts/ui-chart-base';
@@ -105,18 +105,6 @@ export class KpiBase {
                 // if (options.filter && options.filter.top) {
                 //     data = that._applyTopWithOutGroupings(data, options.filter.top);
                 // }
-
-                if (options.top && (options.top.predefined || options.top.custom)) {
-                    if (options.groupings && options.includeTopGroupingValues) {
-                        data = that._applyTopWithGroupings(data, options.groupings, options.includeTopGroupingValues);
-                    } else {
-                        if ((!options.groupings || !options.groupings.length) && options.frequency) {
-                            data = that._applyTopWithOutGroupings(data, options.frequency, options.top);
-                        }
-                    }
-                }
-
-
                 resolve(data);
             }, (e) => {
                 reject(e);
@@ -464,98 +452,6 @@ export class KpiBase {
                 group._id[camelCase(g)] = '$' + g;
             }
         });
-    }
-
-    private _applyTopWithGroupings(data: any[], groupings: string[], includeTopGroupingValues: string[]): any[] {
-        if (!data || !data.length) { return data; }
-        if (!groupings || !groupings.length) { return data; }
-        if (!includeTopGroupingValues || !includeTopGroupingValues.length) { return data; }
-
-        /**
-         * if not in top n, group by 'Others'
-         * else return same object
-         */
-        const groupedData = groupBy(data, (item) => {
-            const groupField = camelCase(groupings[0]);
-
-            // assign NULL_CATEGORY_REPLACEMENT if object does not contain grouping field
-            if (!item._id[groupField]) {
-                item._id[groupField] = NULL_CATEGORY_REPLACEMENT;
-            }
-
-            // check if the grouping value exist in the top n
-            const topNotExist = item._id ?
-                              includeTopGroupingValues.indexOf(item._id[groupField]) === -1 :
-                              false;
-
-            if (topNotExist) {
-                item._id[groupField] = 'Others';
-                // i.e. Others-2018-01
-                return `${item._id[groupField]}-${item._id.frequency}`;
-            }
-
-            return `${item._id[groupField]}`;
-        });
-
-        data = this._reduceOthersAndTop(groupedData, includeTopGroupingValues);
-
-        return data;
-    }
-
-    private _reduceOthersAndTop(groupedData: any, includeTopGroupingValues: string[]): any[] {
-        for (let i in groupedData) {
-            if (includeTopGroupingValues.indexOf(i) === -1) {
-                // get the total of 'Other' grouping
-                const othersTotal = reduce(groupedData[i], (result: any, item: any) => {
-                    return {
-                        value: result.value + item.value
-                    };
-                });
-
-                // get _id object
-                const { _id } = pick(groupedData[i][0], '_id');
-                // show the _id object with the total by frequency of 'Others'
-                groupedData[i] = [{
-                    _id,
-                    value: othersTotal.value
-                }];
-            }
-        }
-
-        return this._reformatTopAndOthers(groupedData);
-    }
-
-    private _reformatTopAndOthers(groupedData: any): any[] {
-        const structuredData = [];
-        // restructure the data back to its original format
-        for (let k in groupedData) {
-            structuredData.push(groupedData[k]);
-        }
-        // convert to single arrary of objects
-        return flatten(structuredData);
-    }
-
-    private _applyTopWithOutGroupings(data: any[], frequency: number, top: IChartTop): any[] {
-        // validate if data array has elements
-        if (!data || data.length === 0) {
-            return data;
-        }
-        if (!top || (!top.predefined && !top.custom)) {
-            return data;
-        }
-
-        const topValue: number = chartTopValue(top);
-        const sortByValue: any[] = sortBy(data, 'value');
-
-        const topNData: any[] = sortByValue.slice(0, topValue);
-        const that = this;
-
-        data = topNData.sort((a: any, b: any) => {
-            const momentFormat = chartTopMomentFormat(frequency);
-            return moment(a, momentFormat).diff(moment(b, momentFormat));
-        });
-
-        return data;
     }
 
     private _regexPattern(type: string, value: string) {
