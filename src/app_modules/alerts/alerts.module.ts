1aa20e50634327d152ee285b3bbec995d66eec1b
chris wright <professionalcandrew@gmail.com>
2018-05-03 13:04:15 -0400

    add mutations, queries, and activies for remove alert. modify the alert schema

--- src/app_modules/alerts/alerts.module.ts
+++ src/app_modules/alerts/alerts.module.ts
@@ -2,6 +2,7 @@ import { UpdateAlertMutation } from './mutations/update-alert.mutation';
 import { CreateAlertMutation } from './mutations/create-alert.mutation';
 import { AlertByWidgetIdQuery } from './queries/alert-by-widget-id.query';
 import { AppModule, ModuleBase } from '../../framework/decorators/app-module';
+import {RemoveAlertMutation} from './mutations/remove-alert.mutation';
 
 @AppModule({
     queries: [
@@ -9,7 +10,8 @@ import { AppModule, ModuleBase } from '../../framework/decorators/app-module';
     ],
     mutations: [
         CreateAlertMutation,
-        UpdateAlertMutation
+        UpdateAlertMutation,
+        RemoveAlertMutation
     ]
 })
 
---
+++ src/app_modules/alerts/mutations/update-alert-active.mutation.ts
@@ -0,0 +1,37 @@
+import { IAlertDocument, IAlertInfo } from '../../../domain/app/alerts/alert';
+import { Alerts } from '../../../domain/app/alerts/alert.model';
+import { AlertInput, AlertMutationResponse } from '../alerts.types';
+import { inject, injectable } from 'inversify';
+import { field } from '../../../framework/decorators/field.decorator';
+import { mutation } from '../../../framework/decorators/mutation.decorator';
+import { MutationBase } from '../../../framework/mutations/mutation-base';
+import { IMutationResponse } from '../../../framework/mutations/mutation-response';
+import { GraphQLTypesMap } from '../../../framework/decorators/graphql-types-map';
+import { UpdateAlertActiveActivity } from '../activities/update-alert-active.activity';
+
+@injectable()
+@mutation({
+    name: 'removeAlert',
+    activity: UpdateAlertActiveActivity,
+    parameters: [
+        { name: 'id', type: GraphQLTypesMap.String, required: true },
+        { name: 'active', type: GraphQLTypesMap.Boolean, required: true }
+    ],
+    output: { type: AlertMutationResponse }
+})
+export class UpdateAlertActiveMutation extends MutationBase<IMutationResponse> {
+    constructor(@inject(Alerts.name) private _alert: Alerts) {
+        super();
+    }
+
+    async run(data: { id: string, active: boolean}): Promise<IMutationResponse> {
+        try {
+            const updateAlert = await this._alert.model.updateAlertActiveField(data.id, data.active);
+            return { success: true, entity: updateAlert };
+        } catch (err) {
+            return {
+                errors: [{ field: '', errors: ['There was error updating the alert active field'] }]
+            };
+        }
+    }
+}
