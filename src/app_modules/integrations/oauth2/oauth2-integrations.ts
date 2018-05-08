a8584d40f8acc79d414023bc158fa0b95774cf31
Raul Vallejo <raul@kpibi.com>
2018-05-08 09:04:09 -0400

    addded a generic error template when integrations fails

--- src/app_modules/integrations/oauth2/oauth2-integrations.ts
+++ src/app_modules/integrations/oauth2/oauth2-integrations.ts
@@ -3,67 +3,56 @@ import { Response } from 'express';
 import { IExtendedRequest } from '../../../middlewares/extended-request';
 import { IntegrationController } from './oauth2-integration-controller';
 import { JsSafeString } from '../../../helpers/string.helpers';
+import * as Handlebars from 'handlebars';
+import * as fs from 'fs';
 
-export function handleOAuth2Itegration(req: IExtendedRequest, res: Response) {
+const genericErrorPage = fs.readFileSync(__dirname + '/templates/generic-integration-error.hbs', 'utf8');
+
+export async function handleOAuth2Itegration(req: IExtendedRequest, res: Response) {
     const logger = req.logger;
 
     logger.debug('processing an oauth2 integration... ');
 
     if (req.query.hasOwnProperty('error') && req.query.error === 'access_denied') {
-        res.send(`
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <script>
-                    window.close();
-                </script>
-            </head>
-            <body>
-            </body>
-            </html>`);
-            return;
+        return res.status(200).send(genericErrorPage);
     }
 
     // code flow authentication only at this point
     if (!req.query.code || !req.query.state) {
-        return res.status(401).json({ error: 'invalid query string' }).end();
+        // return res.status(401).json({ error: 'invalid query string' }).end();
+        return res.status(500).send(genericErrorPage);
     }
 
     const integrationController = req.Container.instance.get<IntegrationController>(IntegrationController.name);
 
     if (!integrationController) {
-            const err = 'something went wrong processing the integration...';
-            logger.error(err);
-            res.status(500).send(err);
-        }
+        const err = 'something went wrong processing the integration...';
+        logger.error(err);
+        return res.status(200).send(genericErrorPage);
+    }
 
-        integrationController.initialize().then(() => {
-            integrationController.executeFlow(req.originalUrl).then(result => {
-                if (result.success) {
-                    res.send(`
-                        <!DOCTYPE html>
-                        <html>
-                        <head>
-                            <script>
-                                window.opener.postMessage({messageSource: 'atlasKPIIntegrations', connectorName: '${JsSafeString(result.connector.name)}', success: true }, '*');
-                                window.close();
-                            </script>
-                        </head>
-                        <body>
-                        </body>
-                        </html>`);
-                    return;
-                }
-            })
-            .catch(err => {
-                logger.error(err);
-                res.status(500).send(err);
-                return;
-            });
-        })
-        .catch(err => {
-            logger.error(err);
-            res.status(500).send(err);
+    try {
+        await integrationController.initialize();
+        const result = await integrationController.executeFlow(req.originalUrl);
+
+        if (result.success) {
+            res.send(`
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <script>
+                        window.opener.postMessage({messageSource: 'atlasKPIIntegrations', connectorName: '${JsSafeString(result.connector.name)}', success: true }, '*');
+                        window.close();
+                    </script>
+                </head>
+                <body>
+                </body>
+                </html>`);
             return;
-        });
+        }
+    } catch (err) {
+        logger.error(err);
+        // res.status(500).send(err);
+        return res.status(500).send(genericErrorPage);
+    }
 }
