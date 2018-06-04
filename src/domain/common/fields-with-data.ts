c920e6a22fc6751bbffa401207f2297bcabf9718
chris wright <professionalcandrew@gmail.com>
2018-06-04 17:20:58 -0400

    update fields with data file to fix fields return to client with data

--- src/domain/common/fields-with-data.ts
+++ src/domain/common/fields-with-data.ts
@@ -1,24 +1,15 @@
-import { isObject } from 'lodash';
-import {DataSourceField} from '../../app_modules/data-sources/data-sources.types';
-import {IValueName} from './value-name';
+import { DataSourceField } from '../../app_modules/data-sources/data-sources.types';
+import { IValueName } from './value-name';
 import { Container } from 'inversify';
-import { isEmpty, isNull } from 'lodash';
+import { isObject, isEmpty, isNull } from 'lodash';
 import {IObject} from '../../app_modules/shared/criteria.plugin';
-import {Aggregate} from 'mongoose';
+import * as Bluebird from 'bluebird';
 
 export const blackListDataSource = ['GoogleAnalytics'];
 
-interface IAggregateMatchQuery {
-    $or: any[];
-}
-
-interface IAggregateProjectQuery extends IObject {
-    _id: number;
-}
-
-interface IAggregateQuery {
-    $match?: IAggregateMatchQuery;
-    $project?: IAggregateProjectQuery;
+interface ICollectionAggregation {
+    $match?: IObject;
+    $project?: IObject;
     $limit?: number;
 }
 
@@ -30,53 +21,50 @@ export async function getFieldsWithData(container: Container, dataSource: string
 
         const model = (container.get(dataSource) as any).model;
         let fieldsWithData: string[] = [];
-        const aggregateQuery: IAggregateQuery[] = [
-            {
-                '$match': { $or: [] }
-            }, {
-                '$project': { _id: 0 }
-            }, {
-                '$limit': 1
-        }];
+
+        const collectionAggregations = [];
         let notIn = { '$nin': ['', null, 'null', 'undefined'] };
 
         if (!model) {
             return [];
         }
 
-        let matchStage = findStage(aggregateQuery, '$match');
-
-        if (!isEmpty(collectionSource)) {
-            Object.assign(matchStage.$match, {
-                source: {
-                    '$in': collectionSource
-                }
-            });
-        }
-
-        fields.forEach((field: DataSourceField|IValueName) => {
-            // i.e referrable.name
+        fields.forEach(field => {
             const fieldPath: string = (field as DataSourceField).path || (field as IValueName).value;
-            // Referral
             const fieldName: string = field.name;
 
-            matchStage.$match.$or.push({
-                [fieldPath]: notIn
-            });
 
-            let projectStage = findStage(aggregateQuery, '$project');
-            Object.assign(projectStage.$project, {
-                [fieldName]: `$${fieldPath}`
-            });
+            const matchOptions = {
+                '$match': {
+                    [fieldPath]: notIn
+                }
+            };
+            if (!isEmpty(collectionSource)) {
+                Object.assign(matchOptions.$match, {
+                    source: {
+                        '$in': collectionSource
+                    }
+                });
+            }
+            const modelAggregate: ICollectionAggregation[] = [
+                matchOptions
+                , {
+                    '$project': {
+                        [fieldName]: fieldPath
+                    }
+                }, {
+                    '$limit': 1
+                }];
 
+            collectionAggregations.push(
+                model.aggregate(modelAggregate)
+            );
         });
 
-        const fieldsExists = await aggregateResponse(model.aggregate(aggregateQuery));
-        if (fieldsExists) {
-            const formatToObject = transformToObject(fieldsExists);
+        fieldsWithData = await Bluebird.all(collectionAggregations);
+        if (fieldsWithData) {
+            const formatToObject = transformToObject(fieldsWithData);
             fieldsWithData = Object.keys(formatToObject);
-
-            return fieldsWithData;
         }
 
         return fieldsWithData;
@@ -85,49 +73,19 @@ export async function getFieldsWithData(container: Container, dataSource: string
     }
 }
 
-function aggregateResponse(aggregate): Promise<Aggregate<Object[]>> {
-    return new Promise<any>((resolve, reject) => {
-        if (!aggregate || !aggregate.exec) {
-            resolve([]);
-            return;
-        }
-        aggregate.exec((err, data) => {
-            resolve(data);
-            return;
-        }, (e) => {
-            reject(e);
-        });
-    });
-}
-
-
+// i.e [ [], [{ [key: string]: any}] ]
 export function transformToObject(arr: any[]): any {
     if (!arr) { return; }
     const newObject = {};
 
-    arr.forEach(item => {
-        if (isObject(item)) {
-            const temp = {};
-            Object.keys(item).forEach(key => {
-                let value = item[key];
-                if (!isNull(value)) {
-                    if (value.match(/\:/)) {
-                        const tempValue = value.split(/\:/);
-                        if (Array.isArray(tempValue) && !isEmpty(tempValue[0])) {
-                            value = tempValue[0];
-                        }
-                    }
-                    temp[key] = value;
+    arr.forEach((singleArray) => {
+        if (singleArray && Array.isArray(singleArray)) {
+            singleArray.forEach(obj => {
+                if (isObject(obj)) {
+                    Object.assign(newObject, obj);
                 }
             });
-            Object.assign(newObject, temp);
         }
-
     });
-
     return newObject;
-}
-
-function findStage(aggregate: IAggregateQuery[], field: string): IAggregateQuery {
-    return aggregate.find(agg => agg[field] !== undefined);
 }
