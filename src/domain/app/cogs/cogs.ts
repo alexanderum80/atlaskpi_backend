1222719ce3318cd1ea7767253c9924e075c6a21b
Raul Vallejo <raul@kpibi.com>
2018-06-05 19:04:07 -0400

    adding payment model to backend

---
+++ src/domain/app/cogs/cogs.ts
@@ -0,0 +1,25 @@
+import * as mongoose from 'mongoose';
+
+import { IEntity, ISaleCustomer, ISaleEmployee, ISaleProduct } from '../sales/sale';
+
+export interface ICOGSProduct extends ISaleProduct {
+    unitCost: number;
+    totalCost: number;
+    profit: number;
+}
+
+export interface ICOGS extends IEntity {
+    billId: string;
+    billDate: Date;
+    chargeId: string;
+    chargeDate: Date;
+    location: Location;
+    customer: ISaleCustomer;
+    employee: ISaleEmployee;
+    category: IEntity;
+    product: ICOGSProduct;
+}
+
+export interface ICOGSDocument extends ICOGS, mongoose.Document { }
+
+export interface ICOGSModel extends mongoose.Model<ICOGSDocument> {
