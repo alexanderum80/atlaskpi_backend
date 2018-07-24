d2670d1cee4c20049973dd5918cc4db648370623
jsdevenv <jsdevenv@bitbucket.org>
2018-07-24 11:27:53 -0400

    pull of development

--- src/services/queues/ga-jobs-queue.service.ts
+++ src/services/queues/ga-jobs-queue.service.ts
@@ -45,9 +45,9 @@ export interface IGAJobData {
 
 
 
-// // queue.app.listen(4000);
+// queue.app.listen(4000);
 
-// const JOB_TYPE: string = 'ga';
+const JOB_TYPE: string = 'ga';
 
 @injectable()
 export class GAJobsQueueService {
@@ -58,7 +58,7 @@ export class GAJobsQueueService {
         @inject(AppConnectionPool.name) private _connPool: AppConnectionPool
         // @inject(GoogleAnalyticsKPIService.name) private _gaKpiService: GoogleAnalyticsKPIService
     ) {
-        this._startProcessingJobs();
+        // this._startProcessingJobs();
     }
 
     addGAJob(
@@ -140,3 +140,146 @@ export class GAJobsQueueService {
     }
 
 }
+
+// import { inject, injectable } from 'inversify';
+// import { Job, Queue } from 'kue';
+// import * as moment from 'moment';
+// import * as os from 'os';
+
+// import { config } from '../../configuration/config';
+// import { IDateRange } from '../../domain/common/date-range';
+// import { FrequencyEnum } from '../../domain/common/frequency-enum';
+// import { GoogleAnalyticsKPIService } from '../kpis/google-analytics-kpi/google-analytics-kpi.service';
+// import { AppConnectionPool } from '../../middlewares/app-connection-pool';
+// import { AppConnection } from '../../domain/app/app.connection';
+// import { GoogleAnalytics } from '../../domain/app/google-analytics/google-analytics.model';
+// import { Connectors } from '../../domain/master/connectors/connector.model';
+
+// // let queue = require('kue');
+
+// export interface IGAJobData {
+//     accountName: string;
+//     dbUri: string;
+//     dataSource: string;
+//     dateRange: IDateRange[];
+//     metric: string;
+//     frequency: FrequencyEnum;
+//     filters: string;
+//     groupings: string[];
+// }
+
+// let queue = require('kue');
+// // let Queue = kue.createQueue();
+// // let _jobs: Queue;
+
+// // try {
+// //     _jobs = queue.createQueue({
+// //         prefix: os.hostname, // 'webapp',
+// //         redis: {
+// //             port: config.cache.redisPort, // 6379,
+// //             host: config.cache.redisServer, // 'localhost'
+// //         }
+// //     });
+
+// //     console.log('Queue name: ' + os.hostname);
+// // } catch (e) {
+// //     console.error('There was an error connecting to Redis Server');
+// // }
+
+
+
+// // queue.app.listen(4000);
+
+// const JOB_TYPE: string = 'ga';
+
+// @injectable()
+// export class GAJobsQueueService {
+//     private _lastTime: moment.Moment;
+
+//     constructor(
+//         @inject(Connectors.name) private _connectors: Connectors,
+//         @inject(AppConnectionPool.name) private _connPool: AppConnectionPool
+//         // @inject(GoogleAnalyticsKPIService.name) private _gaKpiService: GoogleAnalyticsKPIService
+//     ) {
+//         // this._startProcessingJobs();
+//     }
+
+//     // addGAJob(
+//     //     accountName: string,
+//     //     dbUri: string,
+//     //     dataSource: string,
+//     //     dateRange: IDateRange[],
+//     //     metric: string,
+//     //     frequency: FrequencyEnum,
+//     //     filters: string,
+//     //     groupings: string[]): Job {
+//     //         // this._lastTime = this._lastTime.add(10, 'seconds');
+//     //         // if (!_jobs) {
+//     //         //     console.log('Job queue is not ready to receive new jobs');
+//     //         //     return;
+//     //         // }
+
+//     //         const jobData = {
+//     //             title: `Google Analytics Query for:${accountName}, date range: ${JSON.stringify(dateRange)}`,
+//     //             accountName, dbUri, dataSource, dateRange, metric, frequency, filters, groupings };
+
+//     //             const job = _jobs.createJob(JOB_TYPE, jobData);
+
+//     //         // return new Promise<Job>((resolve, reject) => {
+//     //             // console.log(`Run at: ` + this._lastTime.format('HH:mm:ss'));
+//     //             // const job = _jobs.createJob(JOB_TYPE, jobData)
+//     //             job.save(function(err: any, data: any) {
+//     //                 if (err) {
+//     //                     console.error('There was an error creating job');
+//     //                 }
+//     //                 else {
+//     //                     console.log(`Job created, name: "ga", data: ${JSON.stringify(jobData)}`);
+//     //                 }
+//     //             });
+//     //         // });
+//     //     return job;
+//     // }
+
+//     // private _startProcessingJobs() {
+//     //     const that = this;
+//     //     this._lastTime = moment();
+
+//     //     if (!_jobs) {
+//     //         console.log('Job queue is not ready to proccess new jobs');
+//     //         return;
+//     //     }
+
+//     //     _jobs.process(JOB_TYPE, async function(job: Job, done) {
+//     //         const data: IGAJobData = job.data;
+
+//     //         try {
+//     //             // get the instances that we need
+//     //             const conn = await that._connPool.getConnection(job.data.dbUri);
+//     //             const appConnection = new AppConnection({ appConnection: conn } as any);
+//     //             const ga = new GoogleAnalytics(appConnection);
+//     //             const gaKpiService = new GoogleAnalyticsKPIService(ga, that._connectors);
+
+//     //             const dr = data.dateRange && data.dateRange[0];
+//     //             const startDate = moment(dr.from).format('YYYY-MM-DD');
+//     //             const endDate = moment(dr.to).format('YYYY-MM-DD');
+//     //             const gaRes = await gaKpiService.initializeConnection(data.dataSource);
+//     //             const jobId = `${job.id}.${moment().unix()}`;
+//     //             const result = await gaKpiService.cacheData(
+//     //                                 jobId,
+//     //                                 gaRes.analytics,
+//     //                                 gaRes.authClient,
+//     //                                 startDate,
+//     //                                 endDate,
+//     //                                 data.metric,
+//     //                                 data.frequency,
+//     //                                 data.filters,
+//     //                                 data.groupings);
+
+//     //             done(null, result);
+//     //         } catch (e) {
+//     //             done(e);
+//     //         }
+//     //     });
+//     // }
+
+// }
