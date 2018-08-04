1c84bb1df28a4b25548c169147a528596164e019
Orlando Quero <alexanderum80@bitbucket.org>
2018-08-03 23:00:27 -0400

    fixing error on widgets that do not show data for complex kpi

--- src/domain/app/widgets/numeric-widget.ts
+++ src/domain/app/widgets/numeric-widget.ts
@@ -110,8 +110,7 @@ export class NumericWidget extends UIWidgetBase implements IUIWidget {
 
     private async _getKpiData(kpi: IKpiBase, dateRange: IDateRange): Promise<any> {
         try {
-            const kpiClone = cloneDeep(kpi);
-            const result = await kpiClone.getData([dateRange], { filter: null });
+            const result = await kpi.getData([dateRange], { filter: null });
 
             if (result && result.length > 0) {
                 return result[0].value;
