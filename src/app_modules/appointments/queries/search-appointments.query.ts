c29912fed415cb31f49ceba89bd9f199bdfe6c7b
Raul Vallejo <raul@kpibi.com>
2018-10-12 06:43:45 +1100

    appointment query based on users timezone

--- src/app_modules/appointments/queries/search-appointments.query.ts
+++ src/app_modules/appointments/queries/search-appointments.query.ts
@@ -7,6 +7,7 @@ import { Appointment } from '../appointments.types';
 import { IQuery } from './../../../framework/queries/query';
 import { SearchAppointmentsActivity } from './../activities/search-appointments.activity';
 import { SearchAppointmentCriteriaInput } from './../appointments.types';
+import { CurrentUser } from '../../../domain/app/current-user';
 
 @injectable()
 @query({
@@ -19,10 +20,12 @@ import { SearchAppointmentCriteriaInput } from './../appointments.types';
 })
 export class SearchAppointmentsQuery implements IQuery<IAppointment[]> {
     constructor(
-        @inject(Appointments.name) private _appointments: Appointments
+        @inject(Appointments.name) private _appointments: Appointments,
+        @inject(CurrentUser.name) private _user: CurrentUser
     ) { }
 
     run(data: { criteria: SearchAppointmentCriteriaInput }): Promise<IAppointment[]> {
+        data.criteria.timezone = this._user.get().profile.timezone;
         return this._appointments.model.search(data.criteria);
     }
 }
