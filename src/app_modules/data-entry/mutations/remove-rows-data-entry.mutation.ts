973b151130877715eda5623dd96f78695c13ace4
Beatriz Hernandez <bhern104@fiu.edu>
2019-02-11 15:13:09 -0500

    removing zombie code

--- src/app_modules/data-entry/mutations/remove-rows-data-entry.mutation.ts
+++ src/app_modules/data-entry/mutations/remove-rows-data-entry.mutation.ts
@@ -20,7 +20,6 @@ import { RemoveRowsDataEntryCollectionActivity } from '../activities/remove-rows
 })
 export class RemoveRowsDataEntryMutation extends MutationBase<IMutationResponse> {
     constructor(
-        // @inject(DataSourcesService.name) private _dataSourcesSvc: DataSourcesService,
         @inject(VirtualSources.name) private _virtualSourceSvc: VirtualSources,
     ) {
     super();
@@ -28,45 +27,15 @@ export class RemoveRowsDataEntryMutation extends MutationBase<IMutationResponse>
 
     async run(data: { VsId: string, rowsIds: string[] }): Promise<IMutationResponse> {
         try {
-            // const dataInput = JSON.parse(data.input);
             const vs = await this._virtualSourceSvc.model.findOne({ _id: data.VsId});
             const model = vs.collection.conn.model(vs.name, new Schema({}, {strict: false}), vs.source);
 
-            //const transformedRecords = dataInput.map( record => transformFieldsToRealTypes( record , vs.fieldsMap))
-        
-
             await model.remove({ _id: { $in: data.rowsIds } });
-            //await model.insertMany(transformedRecords);
 
             return { success: true };
         } catch (e) {
             console.error(e);
             return { success: false };
         }
-
-        // const that = this;
-        // return new Promise<IMutationResponse>((resolve, reject) => {
-        //     const dataInput = JSON.parse(data.input);
-
-
-
-        //     const fileExtensionIndex = dataInput.inputName.lastIndexOf('.') !== -1 ? dataInput.inputName.lastIndexOf('.') : dataInput.inputName.length;
-        //     dataInput.inputName = camelCase(dataInput.inputName.substr(0, fileExtensionIndex)).toLowerCase();
-
-        //     this._virtualSourceSvc.model.getDataSourceByName(dataInput.inputName).then(virtualSource => {
-        //         that._dataSourcesSvc.removeVirtualSourceMapCollection(virtualSource.source).then(() => {
-        //             dataInput.inputName = virtualSource.source;
-        //             dataInput.fields = JSON.parse(dataInput.fields);
-        //             dataInput.records = JSON.parse(dataInput.records);
-
-        //             that._dataSourcesSvc.createVirtualSourceMapCollection(dataInput).then(() => {
-        //                 resolve({ success: true });
-        //                 return;
-        //             });
-        //         });
-        //     }).catch(err => {
-        //         resolve(null);
-        //     });
-        // });
     }
 }
